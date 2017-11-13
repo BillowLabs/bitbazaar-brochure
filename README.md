a8080183ff97f7cff1ed5c4c8ca446446f70abfb
Michael Huensch <setup@run00.com>
2017-11-12 20:55:55 -0600

    Adding coming soon

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# bitbrochure
+# bitbazaar
+
+> Nuxt.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+$ npm install # Or yarn install
+
+# serve with hot reload at localhost:3000
+$ npm run dev
+
+# build for production and launch server
+$ npm run build
+$ npm start
+
+# generate static project
+$ npm run generate
+```
+
+A hook has been added to git so that the static content is always generated on commit and placed in the "docs" folder.  This makes the site easy to host on GitHub from that folder as the root of the site.
+
+For detailed explanation on how things work, checkout the [Nuxt.js docs](https://github.com/nuxt/nuxt.js).
---
+++ pages/index.vue
@@ -0,0 +1,61 @@
+<template>
+  <section class="container">
+    <div>
+      <!-- <logo/> -->
+      <h1 class="title">
+        bitbazaar
+      </h1>
+      <h2 class="subtitle">
+        A New Kind of <br />
+        Cryptocurrency Exchange!<br/>
+        [comming soon]
+      </h2>
+
+      <div class="links">
+        Want to learn more about cryptocurrencies?<br/><br/>
+        <a href="http://book.bitbazaar.io" target="_blank" class="button--green">Checkout Our Free Book</a>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 60px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 22px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
